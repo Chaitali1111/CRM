bea0aa3e693b90960ba9d336daa563d9ec00fe4d
Chaitali1111 <166948376+Chaitali1111@users.noreply.github.com>
2026-06-30 20:05:12 +0530

    Docker support for Selenium

--- app/google_maps.py
+++ app/google_maps.py
@@ -1,10 +1,6 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
-
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-
 import time
 from app.models import Lead
 
@@ -16,10 +12,19 @@ def search_google_maps(keyword, category, group_name, db):
     print("Group   :", group_name)
 
     # Start Chrome
-    driver = webdriver.Chrome(
-        service=Service(ChromeDriverManager().install())
-    )
+    from selenium.webdriver.chrome.options import Options
+    import os
+    options = Options()
 
+    options.binary_location = os.getenv("CHROME_BIN", "/usr/bin/chromium")
+
+    options.add_argument("--headless=new")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--window-size=1920,1080")
+
+    driver = webdriver.Chrome(options=options)
     driver.maximize_window()
 
     # Open Google Maps
